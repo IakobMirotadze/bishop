1baeb9bdf77480fd38d0cb796175d93f5ea3f2d6
Iakobi <iakobi.mirotadze@gmail.com>
2022-10-22 19:55:17 +0400

    updated config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 const jsonConfig = {
 
-    API_URL: "https://estores.ge/api",
+    API_URL: "http://estores.ge/api",
     WEBSITE_URL: "https://www.estores.ge",
     IMG_URL: "https://estores.ge/api/",
 
